3f0e3e092c0dacea591df28551f8360afb678f02
Olena Nesteruk <pavlena81@gmail.com>
2022-12-07 23:13:42 +0200

    classicTypes

--- app.ts
+++ app.ts
@@ -1,3 +1,5 @@
+import { Console } from "console";
+
 const button = document.querySelector("button");
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
@@ -9,3 +11,21 @@ function add(num1: number, num2: number) {
 button!.addEventListener("click", function() {
   console.log(add(+input1.value, +input2.value));
 });
+
+//Задайте правильные ts типы, для классических js;
+let age: number;
+let name: string;
+
+let toggle: boolean;
+let empty: null;
+let notInitialize: undefined;
+let callback = (a) => { return 100 + a };
+//
+
+// И назначаем
+age = 50;
+name = 'Max';
+console.log(name);
+toggle = true;
+empty = null;
+notInitialize = undefined;
